bf5b72e007906911dc43111e974c38f2612a9045
tom138 <82599643+tkdcksqkr98@users.noreply.github.com>
2023-03-28 18:14:49 +0900

    Spring02
    spring~~~ing

---
+++ spring01/src/main/java/com/multi/mvc01/MemberVO.java
@@ -0,0 +1,45 @@
+package com.multi.mvc01;
+
+public class MemberVO {
+	//MemberV0가방에 넣은 데이터는 Member테이블에 들어갈 예정
+	//각 컬럼과 일치시켜 줌.
+	private String id;
+	private String pw ;
+	private String name;
+	private String tel;
+	
+	public String getId() {
+		return id;
+	}
+
+	/*
+	 * @Override public String toString() { return "MemberVO [id=" + id + ", pw=" +
+	 * pw + ", name=" + name + ", tel=" + tel + "]"; }
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+	
+	
+	
+}
