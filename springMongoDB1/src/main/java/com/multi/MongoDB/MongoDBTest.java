a509f56d1a8f0a56c40307afe1cd4839c9aee24f
tom138 <82599643+tkdcksqkr98@users.noreply.github.com>
2023-04-26 20:03:21 +0900

    Spring
    MongoDB로 셋팅 및 Studio3T 연결, CRUD해보기

---
+++ springMongoDB1/src/main/java/com/multi/MongoDB/MongoDBTest.java
@@ -0,0 +1,17 @@
+package com.multi.MongoDB;
+
+import com.mongodb.MongoClient;
+
+public class MongoDBTest {
+
+	public static void main(String[] args) {
+		 //1. 몽고db에 프로그램에 연결
+		 MongoClient client = new MongoClient("localhost", 27017);
+		 System.out.println("1.몽고 db프로그램에 연결 성공");
+		 //2. shop2 db로 연결
+		 
+		 //3. member 컬렉션에 연결
+
+	}
+
+}
